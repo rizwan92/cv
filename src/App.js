82223017a265bdce9335addce2603d772dd42644
Rizwan Chouhan <akshay@Akshays-MacBook-Pro.local>
2019-07-29 14:26:15 +0530

    update the cv

--- src/App.js
+++ src/App.js
@@ -10,7 +10,7 @@ class App extends Component {
   render() {
 
     const person = {
-      avatar: 'https://media.licdn.com/dms/image/C5103AQH_6NWEqKADFg/profile-displayphoto-shrink_200_200/0?e=1542844800&v=beta&t=hTtckz-bK-mvVSuRlvN_uObkaBeFNVw6LBSYkXGtFks',
+      avatar: 'https://res.cloudinary.com/dcr2pfgxy/image/upload/v1564387487/mystuff/rizwan.jpg',
       name: 'RIZWAN CHOUHAN',
       profession: 'Full Stack Javascript Developer',
       bio: '❤ Mobile App Developer - React-Native Mobile App Development',
@@ -19,20 +19,28 @@ class App extends Component {
         {name: 'facebook', url: 'https://www.facebook.com/rizwan.chouhan.52'},
         {name: 'twitter', url: 'https://twitter.com/rizwan_chouhan'},
         {name: 'github', url: 'https://github.com/rizwan92'},
-        {name: 'linkedin', url: 'https://www.linkedin.com/in/md-rizwan-raza-9b1770117/'}
+        {name: 'linkedin', url: 'https://www.linkedin.com/in/rizwan92'}
       ],
       experience: [
-        {jobTitle: 'Front End', startDate: 'November 2015', endDate: 'Present', jobDescription: 'Using React, Apollo Client, HTML, CSS, Flexbox, Jquery, Twitter Bootstrap, Semantic UI and  Matrial Design as my Front-End Stacks'},
-        {jobTitle: 'Backend', startDate: 'November 2015', endDate: 'Present', jobDescription: 'Nodejs, Express, Meteor, PHP, AWSCLI, GraphQl, Hyperledger Composer, Ethereum Solidity.'},
-        {jobTitle: 'Mobile App', startDate: 'July 2016', endDate: 'Present', jobDescription: 'Android (Java) , React-Native '},
-        {jobTitle: 'Database', startDate: 'November 2015', endDate: 'Present', jobDescription: 'MYSQL, PostgreSQL, MongoDb, (Started Cloud MongoDb as Database sevice in September 2018)'},
-        {jobTitle: 'Others',endDate: 'Present', jobDescription: 'Linux(Ubuntu), Docker,'},
+        {jobTitle: 'Front End', startDate: '', endDate: '', jobDescription: 'Using React, Apollo Client, HTML, CSS, Flexbox, Jquery, Twitter Bootstrap, Semantic UI, Bulma, and  Matrial Design as my Front-End Stacks'},
+        {jobTitle: 'Backend', startDate: '', endDate: '', jobDescription: 'Nodejs, Express, Meteor, PHP, AWSCLI, GraphQl, Hyperledger Composer, Ethereum Solidity.'},
+        {jobTitle: 'Mobile App', startDate: '', endDate: '', jobDescription: 'Android (Java) , React-Native '},
+        {jobTitle: 'Database', startDate: '', endDate: '', jobDescription: 'MYSQL, PostgreSQL, MongoDb, (Started Cloud MongoDb as Database sevice in September 2018)'},
+        {jobTitle: 'Others',endDate: '', jobDescription: 'Linux(Ubuntu), Docker, Cloud'},
       ],
-      projects: [
-        {jobTitle: 'Sandwich Junction', company:'Metoer (NodeJs)', startDate: 'Feb 2017', endDate: 'April 2017', jobDescription: 'Sandwich Junction is a leading business in sandwich service Supply Chain in Raipur. Developed from billing to supply and management event driven based system. Developed on Meteor Framework.',link:['http://52.66.26.168/login']},
+      exp: [
+        {jobTitle: 'Project Manager', company:'Traddle Pvt Ltd', startDate: 'July 2018', endDate: 'Present', jobDescription: 'Designing, Implementing, Deploying and Maintaining the existing system in Clouds in order to help your organization to achieve its goals.',link:['https://traddle.in']},
+        {jobTitle: 'Hyperledger fabric system developer', company:'Blockright', startDate: 'July 2018', endDate: 'Present', jobDescription: 'Senior Blockchain Developer, Hyperledger fabric system developer. Solving Problems Using Blockchain Technology.',link:['https://www.facebook.com/blockright']},
+        {jobTitle: 'Project Manger', company:'Mongo Bhai', startDate: 'July 2018', endDate: 'Present', jobDescription: 'Mongo Bhai, MongoDB as Database Service built on AWS for the developer.',link:['http://mongobhai.com']},
+        {jobTitle: 'Mobile Application Developer (Freelancer)', company:'Wergo.social', startDate: 'July 2017', endDate: 'July 2018', jobDescription: 'Developing Mobile Application for both Android and IOS and React-Native Tech Stack: React-Native, Redux, Redux Thunk, Firebase.',link:['https://wergo.social']},
+        {jobTitle: 'Mobile Application Developer (Freelancer)', company:'Click IPO', startDate: 'July 2016', endDate: 'July 2017', jobDescription: 'Developing Mobile Application for both Android and IOS and React-Native Tech Stack: React-Native, Redux, Redux Thunk, Firebase.',link:['https://clickipo.com']},
+      ],
+      project: [
+        {jobTitle: 'Advocate General Office Chhattisgarh' , company:'Bilaspur High Court', startDate: '', endDate: '', jobDescription: 'Internal Management System for advocate general office Bilaspur chhattisgarh',link:['http://cgadvocategeneral.gov.in']},
+        {jobTitle: 'Atithi Complaince System' , company:'Blockchain Innovation', startDate: '', endDate: '', jobDescription: 'Blockchain based compliance solution to power smart cities. ',link:['https://atithi.app/']},
+        {jobTitle: 'Sandwich Junction', company:'Metoer (NodeJs)', startDate: 'Feb 2017', endDate: 'April 2017', jobDescription: 'Sandwich Junction is a leading business in sandwich service Supply Chain in Raipur. Developed from billing to supply and management event driven based system. Developed on Meteor Framework.',link:['http://mysandwichjunction.com']},
         {jobTitle: 'Bihan Application', company:'React-Native', startDate: 'May 2017', endDate: 'Jun 2017', jobDescription: 'Bihan is the service under NRLM department of chhattisgarh state in India. which helps self help group and other rural development activities in chhattisgarh.',link:['https://play.google.com/store/apps/details?id=com.bihan']},
         {jobTitle: 'Maroon Entertainment Application' , company:'React-Native', startDate: 'Jun 2017', endDate: 'July 2017', jobDescription: 'Maroon entertainment is the event organization and management company. I am working on its ticket booking and management dashboard or ticket booking React-Native Application',link:['https://play.google.com/store/apps/details?id=com.maroon&hl=en']},
-        {jobTitle: 'Atithi Complaince System' , company:'Hyperledger Composer', startDate: 'November 2017', endDate: 'Present', jobDescription: 'Blockchain based compliance solution to power smart cities. ',link:['https://atithi.app/']},
         {jobTitle: 'MDAS ' , company:'Open Source Project', startDate: '2018', jobDescription: 'MongoDb Database As Service in Clouds ',link:['Not Available We Are working on Architecture']},
         {jobTitle: 'Freelancing Websites' , company:'React and PHP', startDate: ' from 2016', jobDescription: 'My Freelance websites and work',link:['http://allcoursesfree.com/','https://rizwan92.github.io/ndax.github.io/index.html','http://nbdigitech.com/','https://rizwan92.github.io/blockcertify/#/','http://letzholiday.in/ref/','https://www.directionclasses.com/']},
       ],
@@ -50,7 +58,9 @@ class App extends Component {
         {name: 'React-Native', percentage: '85%'},
         {name: 'PHP', percentage: '60%'},
         {name: 'Hyperledger Blockchain', percentage:"30%"},
-        {name: 'Ethereum Solidity', percentage:"25%"}
+        {name: 'Ethereum Solidity', percentage:"25%"},
+        {name: 'Docker', percentage:"50%"},
+        {name: 'Cloud', percentage:"50%"}
       ]
     };
 
@@ -73,10 +83,10 @@ class App extends Component {
                 <Education education={person.education} />
                 <Certificate certificate={person.certificate} />
                 <Skills skills={person.skills} />
-                <Projects  experience={person.projects}/>
+                <Projects name="Experience"  experience={person.exp}/>
+                <Projects name="Projects" experience={person.project}/>
               </div>
           </div>
-
         </div>
       </header>
     );
--- src/components/Projects.js
+++ src/components/Projects.js
@@ -3,8 +3,8 @@ import React from 'react';
 const Projects = (props) => {
   const myExperience = (
     <div>
-      {props.experience.map((exp) =>
-        <div className='item' key={exp.jobTitle}>
+      {props.experience.map((exp,i) =>
+        <div className='item' key={i}>
           <h3>{exp.jobTitle} @ {exp.company} <span>{exp.startDate} - {exp.endDate}</span></h3>
           <p>{exp.jobDescription}</p>
           {exp.link.map((lnk,i)=><a style={{display:'block'}} key={i} href={lnk} target="_blank">{lnk}</a>)}
@@ -15,7 +15,7 @@ const Projects = (props) => {
   return (
     <div className='title'>
       <i className='fa fa-briefcase'></i>
-      <h2>Projects</h2>
+      <h2>{props.name}</h2>
       {myExperience}
     </div>
   )
