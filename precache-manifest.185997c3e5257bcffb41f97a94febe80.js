6bfad08736c05ac3da7b787e12c358ccb0e9770e
Rizwan Chouhan <akshay@Akshays-MacBook-Pro.local>
2019-07-29 14:54:20 +0530

    Updates

---
+++ precache-manifest.185997c3e5257bcffb41f97a94febe80.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d1fd1c6fa71a4b64b25c5f4b87baae00",
+    "url": "/cv/index.html"
+  },
+  {
+    "revision": "681b51057d74cfa8975d",
+    "url": "/cv/static/js/2.65aa1cca.chunk.js"
+  },
+  {
+    "revision": "587697ebe34778d5dc90",
+    "url": "/cv/static/js/main.83aa5c8b.chunk.js"
+  },
+  {
+    "revision": "18079a4bdaee1d56978c",
+    "url": "/cv/static/js/runtime~main.6f651ed7.js"
+  }
+]);
